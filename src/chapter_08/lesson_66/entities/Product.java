a30c10794967b1c86180ae9486dc387534d3ddb7
jeffersonrolino <jefferson.rolino@gmail.com>
2024-03-15 11:35:01 -0300

    Capítulo 08 - Aula 66: Começando a resolver um segundo problema exemplo

---
+++ src/chapter_08/lesson_66/entities/Product.java
@@ -0,0 +1,19 @@
+package chapter_08.lesson_66.entities;
+
+public class Product {
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double totalValueInStock(){
+        return price * quantity;
+    }
+
+    public void addProducts(int quantity){
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity){
+        this.quantity -= quantity;
+    }
+}
