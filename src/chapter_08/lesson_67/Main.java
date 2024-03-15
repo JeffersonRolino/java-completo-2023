c63675ac7cacd9f8dc82350cbbe844f2643f0181
jeffersonrolino <jefferson.rolino@gmail.com>
2024-03-15 15:31:22 -0300

    Capítulo 08 - Aula 67: Object e toString()

---
+++ src/chapter_08/lesson_67/Main.java
@@ -0,0 +1,30 @@
+package chapter_08.lesson_67;
+
+import chapter_08.lesson_67.entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+
+        Product product = new Product();
+        System.out.println("Enter product data: ");
+        System.out.println("Name: ");
+        product.name = scanner.nextLine();
+
+        System.out.println("Price: ");
+        product.price = scanner.nextDouble();
+
+        System.out.println("Quantity in Stock: ");
+        product.quantity = scanner.nextInt();
+
+
+        System.out.println(product);
+
+        scanner.close();
+    }
+}
