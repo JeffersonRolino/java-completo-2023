17c1f93c29c55cb6dc64eb8bf3bccd10a3aa19bd
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-02 20:08:01 -0300

    Capítulo 14 - Aula 157: Herança

---
+++ src/chapter_14/lesson_157/application/Main.java
@@ -0,0 +1,10 @@
+package chapter_14.lesson_157.application;
+
+import chapter_14.lesson_157.entities.BusinessAccount;
+
+public class Main {
+    public static void main(String[] args) {
+        BusinessAccount account = new BusinessAccount();
+
+    }
+}
