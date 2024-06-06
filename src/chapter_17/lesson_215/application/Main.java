27b960335114c0be9efdd8bd4aa53fc4a198c72c
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-05 22:56:32 -0300

    Capítulo 17 - Aula 215: Lendo arquivo de texto com classes File e Scanner

---
+++ src/chapter_17/lesson_215/application/Main.java
@@ -0,0 +1,27 @@
+package chapter_17.lesson_215.application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("c:\\temp\\in.txt");
+        Scanner scanner = null;
+
+        try{
+            scanner = new Scanner(file);
+            while (scanner.hasNextLine()){
+                System.out.println(scanner.nextLine());
+            }
+        }
+        catch (IOException exception){
+            System.out.println("Error: " + exception.getMessage());
+        }
+        finally {
+            if(scanner != null){
+                scanner.close();
+            }
+        }
+    }
+}
