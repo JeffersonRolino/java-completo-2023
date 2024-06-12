c97fcc7fd5927518e369b803592cf3cbb36c055d
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-12 08:11:42 -0300

    Capítulo 19 - Aula 239: Introdução aos Generics

---
+++ src/chapter_19/lesson_239/application/Program.java
@@ -0,0 +1,27 @@
+package chapter_19.lesson_239.application;
+
+import chapter_19.lesson_239.services.PrintService;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        PrintService<Integer> ps = new PrintService<>();
+
+        System.out.print("How many values? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int value = sc.nextInt();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        System.out.println("First: " + ps.first());
+
+        sc.close();
+    }
+}
