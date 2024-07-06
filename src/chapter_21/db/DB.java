7e2e17a48e4200ec84b10d748f76345fdc480652
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-06 16:48:59 -0300

    Capítulo 21 - Aula 270: Preparação do primeiro projeto - Parte 2

--- src/chapter_21/db/DB.java
+++ src/chapter_21/db/DB.java
@@ -1,4 +1,50 @@
 package chapter_21.db;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DB {
+
+    private static Connection connection = null;
+
+    private static Properties loadProperties(){
+        try(FileInputStream fileInputStream = new FileInputStream("db.properties")){
+            Properties properties = new Properties();
+            properties.load(fileInputStream);
+            return properties;
+        }
+        catch (IOException ioException){
+            throw new DbException(ioException.getMessage());
+        }
+    }
+
+    public static Connection getConnection() {
+        if(connection == null){
+            Properties properties = loadProperties();
+            String url = properties.getProperty("dburl");
+
+            try {
+                connection = DriverManager.getConnection(url, properties);
+            }
+            catch (SQLException sqlException){
+                throw new DbException(sqlException.getMessage());
+            }
+        }
+
+        return connection;
+    }
+
+    public static void closeConnection(){
+        if(connection != null){
+            try {
+                connection.close();
+            } catch (SQLException exception) {
+                throw new DbException(exception.getMessage());
+            }
+        }
+    }
 }
